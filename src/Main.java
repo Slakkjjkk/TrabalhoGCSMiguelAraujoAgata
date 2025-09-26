7598c477728a63aad95c0a7ae18465c8c9470022
Miguel Araujo <miguelitoaalmeida@gmail.com>
2025-09-26 19:04:15 -0300

    feat: adicionar input com Scanner (branch input-feature)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int a = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int b = sc.nextInt();
         System.out.println("Soma: " + (a + b));
         System.out.println("Subtração: " + (a - b));
         System.out.println("Multiplicação: " + (a * b));
-        System.out.println("Divisão: " + (a / b));
+        if (b != 0) {
+            System.out.println("Divisão: " + (a / b));
+        } else {
+            System.out.println("Não é possível dividir por zero.");
+        }
+        sc.close();
     }
 }
