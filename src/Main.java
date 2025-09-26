6daf48d2487af1e8f942ffb2af20a1a66d3a2f68
Miguel Araujo <miguelitoaalmeida@gmail.com>
2025-09-26 19:01:05 -0300

    feat: adicionar multiplicação e divisão (commit 3)

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         int b = 5;
         System.out.println("Soma: " + (a + b));
         System.out.println("Subtração: " + (a - b));
+        System.out.println("Multiplicação: " + (a * b));
+        System.out.println("Divisão: " + (a / b));
     }
 }
