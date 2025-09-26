563b565390bd0bedba9ea90a9876d005352be570
Miguel Araujo <miguelitoaalmeida@gmail.com>
2025-09-26 18:59:28 -0300

    feat: adicionar subtração (commit 2)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtração: " + (a - b));
+    }
+}
